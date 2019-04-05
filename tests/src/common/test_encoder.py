83bdcfd8ae2a1ceb3a7d5a94b5e5ea057853f644
Abdullah Alhazmy <me@alhazmy13.net>
2019-04-05 06:27:28 +0300

    + add ElasticSearchEncoder test

--- tests/src/common/test_encoder.py
+++ tests/src/common/test_encoder.py
@@ -1,8 +1,9 @@
+import json
 import unittest
 from unittest.mock import MagicMock
 
 from src.app.post.model import PostModel
-from src.common.encoder import PynamoDbEncoder
+from src.common.encoder import ElasticSearchEncoder, PynamoDbEncoder
 
 
 class TestPynamoDbEncoder(unittest.TestCase):
@@ -18,3 +19,18 @@ class TestPynamoDbEncoder(unittest.TestCase):
 
         # then
         self.assertEqual(result, mock_model.attribute_values)
+
+
+class TestElasticSearchEncoder(unittest.TestCase):
+    def test_post_model_response(self):
+        # given
+        mock_model = MagicMock(spec=ElasticSearchEncoder)
+        mock_model.title = "title"
+        mock_model._l_ = {}
+
+        # when
+        encoder = ElasticSearchEncoder()
+        result = encoder.default(mock_model)
+
+        # then
+        self.assertEqual(result, mock_model._l_)
